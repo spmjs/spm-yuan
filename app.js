987c9039e31eaee3d47355e276fa3beb5d89e881
edokeh <edokeh@163.com>
2013-06-13 15:13:18 +0800

    Add an index page to view all modules

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var app = connect.createServer(
     }),
 
     connectRoute(function (router) {
+        router.get('/repository/', actions.getAll);
         router.get('/repository/:family/', actions.getFamily);
         router.get('/repository/:family/:name/', actions.getProject);
         router.get('/repository/:family/:name/:version/', actions.getModule);
@@ -28,11 +29,19 @@ var app = connect.createServer(
         router.delete('/repository/:family/:name/', actions.deleteModule);
 
         router.post('/account/login', actions.login);
+
+        router.get('/', function (req, resp) {
+            resp.writeHead(302, {
+                'Location' : '/assets/index.html'
+            });
+            response.end();
+        })
     }),
 
     connect.errorHandler()
 );
 
 app.use('/repository', connect.static(lib.util.getSpmCacheDir()));
+app.use('/assets', connect.static('assets'));
 
 module.exports = app;
