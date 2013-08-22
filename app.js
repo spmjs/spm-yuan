fa988f722b9e92adc0b6a9131a88639f7532ffdf
edokeh <edokeh@163.com>
2013-08-22 20:55:55 +0800

    Use express.js  #2

--- app.js
+++ app.js
@@ -1,56 +1,7 @@
-//var connect = require('connect');
-//var connectRoute = require('connect-route');
-//var path = require('path');
-//var lib = require('./lib');
-//var actions = lib.actions;
-//
-//var app = connect.createServer(
-//    connect.logger('dev'),
-//    connect.favicon(),
-//    connect.bodyParser(),
-//    connect.query(),
-//
-//    connectRoute(function (router) {
-//        // auth
-//        router.post('/repository/:family/:name/:version/', actions.checkAuth);
-//        router.put('/repository/:family/:name/:version/', actions.checkAuth);
-//        router.delete('/repository/:family/:name/:version/', actions.checkAuth);
-//        router.delete('/repository/:family/:name/', actions.checkAuth);
-//    }),
-//
-//    connectRoute(function (router) {
-//        router.get('/repository/', actions.getAll);
-//        router.get('/repository/:family/', actions.getFamily);
-//        router.get('/repository/:family/:name/', actions.getProject);
-//        router.get('/repository/:family/:name/:version/', actions.getModule);
-//
-//        router.post('/repository/:family/:name/:version/', actions.createModule);
-//        router.put('/repository/:family/:name/:version/', actions.updateModule);
-//        router.delete('/repository/:family/:name/:version/', actions.deleteModule);
-//        router.delete('/repository/:family/:name/', actions.deleteModule);
-//
-//        router.post('/account/login', actions.login);
-//
-//        router.get('/', function (req, resp) {
-//            resp.writeHead(302, {
-//                'Location' : '/assets/index.html'
-//            });
-//            resp.end();
-//        })
-//    }),
-//
-//    connect.errorHandler()
-//);
-//
-//app.use('/repository', connect.static(lib.util.getSpmCacheDir()));
-//app.use('/assets', connect.static(path.join(__dirname, 'assets')));
-//
-//module.exports = app;
-
 var express = require('express');
 var path = require('path');
 var controllers = require('./lib/controllers');
-var authFilter = require('./lib/filter/auth-filter');
+var authFilter = require('./lib/filters/auth-filter');
 var app = express();
 
 app.set('views', __dirname + '/lib/views');
@@ -59,14 +10,18 @@ app.use(express.favicon());
 app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser());
-app.use(express.cookieSession({
-    key : '_spm_yuan_session',
-    secret : 'sb'
-}));
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+// error handler
+app.use(function (error, req, res, next) {
+    res.status(500).json({
+        message : error,
+        status : 'error'
+    });
+});
 
+// router
 app.get('/repository', controllers.family.index);
 app.get('/repository/:family', controllers.family.show);
 app.get('/repository/:family/:name', controllers.project.show);
@@ -79,4 +34,5 @@ app.del('/repository/:family/:name', authFilter, controllers.module.destroy);
 
 app.post('/account/login', controllers.session.create);
 app.get('/', controllers.home.show);
-app.listen(3000)
+
+module.exports = app;
